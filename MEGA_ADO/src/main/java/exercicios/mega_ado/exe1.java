e0f614b812cda386ab79bca049c45f6a6f3d61da
AugustoB1 <guto.brito280802@gmail.com>
2022-05-05 19:56:53 -0300

    Adionamento do exercicios 2 / switch case

--- MEGA_ADO/src/main/java/exercicios/mega_ado/exe1.java
+++ MEGA_ADO/src/main/java/exercicios/mega_ado/exe1.java
@@ -1,27 +1,54 @@
-
 package exercicios.mega_ado;
+
 import java.util.Scanner;
+
 public class exe1 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double temp;
-        String chuva = null;
-        double din = 0;
-        System.out.println("Qual temperatura prevista ?");
-        temp = input.nextDouble();
-        if (temp >= 25) {
-            System.out.println("Tem previsão de chuva?");
-            chuva = input.next();
+        System.out.println("Informe o exercício que deseja ver: ");
+        int exercicios = input.nextInt();
+
+        switch (exercicios) {
+            case 1:
+                double temp;
+                String chuva = null;
+                double din = 0;
+                System.out.println("Qual temperatura prevista ?");
+                temp = input.nextDouble();
+                if (temp >= 25) {
+                    System.out.println("Tem previsão de chuva?");
+                    chuva = input.next();
+                }
+                if ("nao".equals(chuva)) {
+                    System.out.println("Qual valor do orçamento ?");
+                    din = input.nextDouble();
+                }
+                if (din >= 2000) {
+                    System.out.println("Podemos ir para a praia");
+                } else {
+                    System.out.println("Não podemos ir para a praia");
+                    break;
+                }
+
+            case 2:
+                int u;
+                double valorUnitario, valorTotal, desconto;
+
+                System.out.println("Digite a quantidade de itens comprados: ");
+                u = input.nextInt();
+                System.out.println("Digite o valor unitario do item comprado: ");
+                valorUnitario = input.nextFloat();
+                if (u >= 10) {
+                    desconto = (valorUnitario * u) * 0.10;
+                    valorTotal = (valorUnitario * u) - desconto;
+                    System.out.println("O valor total com 10% de desconto: R$" + valorTotal + "(desconto de R$ " +desconto+ ")");
+                } else {
+                    valorTotal = valorUnitario * u;
+                    System.out.println("O valor total sem desconto: R$" + valorTotal);
+                }
+                break;
+
         }
-        if ("nao".equals(chuva)) {
-            System.out.println("Qual valor do orçamento ?");
-             din = input.nextDouble();            
-        }
-        if (din >= 2000) {
-            System.out.println("Podemos ir para a praia");
-        } else {
-            System.out.println("Não podemos ir para a praia");
-        }
-    } 
+    }
 }

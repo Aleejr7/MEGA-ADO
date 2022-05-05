0a770ebb3afc1a6632388bec7360333126fd4d8d
brenno55 <brennolima55@outlook.com>
2022-05-05 20:56:12 -0300

    Adicionamento do exercicio 5 / Correção ao exercício 1

--- MEGA_ADO/src/main/java/exercicios/mega_ado/exe1.java
+++ MEGA_ADO/src/main/java/exercicios/mega_ado/exe1.java
@@ -9,32 +9,47 @@ public class exe1 {
         System.out.println("Informe o exercício que deseja ver: ");
         int exercicios = input.nextInt();
 
-        switch (exercicios) {           
-            case 1:             
+        switch (exercicios) {
+
+            case 1:
+                System.out.println("--- Integrantes do grupo --- \n");
+                System.out.println("Alesandro da Rocha Santos Junior");
+                System.out.println("Augusto Brito Santos");
+                System.out.println("Brenno Lima Ferreira da Silva");
+                System.out.println("Deive Oliveira dos Anjos");
+                break;
+
+            case 2:
+                System.out.println("--- Podemos ir para praia ? --- \n");
                 double temp;
-                String chuva = null;
+                String chuva = "";
                 double din = 0;
                 System.out.println("Qual temperatura prevista ?");
                 temp = input.nextDouble();
                 if (temp >= 25) {
-                    System.out.println("Tem previsão de chuva?");
+                    System.out.println("O clima está: sol, nublado, garoa ou chuva?");
                     chuva = input.next();
-                }
-                if ("nao".equals(chuva)) {
-                    System.out.println("Qual valor do orçamento ?");
-                    din = input.nextDouble();
-                }
-                if (din >= 2000) {
-                    System.out.println("Podemos ir para a praia");
+                    if ("sol".equals(chuva) || "nublado".equals(chuva)) {
+                        System.out.println("Qual valor do orçamento ?");
+                        din = input.nextDouble();
+                        if (din >= 2000) {
+                            System.out.println("Podemos ir para a praia");
+                        } else {
+                            System.out.println("Não podemos ir para a praia");
+                        }
+                    } else {
+                        System.out.println("Não podemos ir para a praia");
+                    }
                 } else {
                     System.out.println("Não podemos ir para a praia");
-                    break;
                 }
+                break;
 
-            case 2:
+            case 3:
+                System.out.println("--- Calcular valor com desconto e sem desconto ---");
                 int u;
-                double valorUnitario, valorTotal, desconto;
-
+                double valorUnitario,
+                 valorTotal, desconto;
                 System.out.println("Digite a quantidade de itens comprados: ");
                 u = input.nextInt();
                 System.out.println("Digite o valor unitario do item comprado: ");
@@ -42,13 +57,34 @@ public class exe1 {
                 if (u >= 10) {
                     desconto = (valorUnitario * u) * 0.10;
                     valorTotal = (valorUnitario * u) - desconto;
-                    System.out.println("O valor total com 10% de desconto: R$" + valorTotal + "(desconto de R$ " +desconto+ ")");
+                    System.out.println("O valor total com 10% de desconto: R$" + valorTotal + "(desconto de R$ " + desconto + ")");
                 } else {
                     valorTotal = valorUnitario * u;
                     System.out.println("O valor total sem desconto: R$" + valorTotal);
                 }
                 break;
 
+            case 5:
+
+                System.out.println("--- Calculo da conta de energia ---");
+                double kwh,
+                 conta;
+                System.out.println("Informe o kwh conaumido: ");
+                kwh = input.nextDouble();
+
+                if (kwh <= 30) {
+                    conta = kwh * 0.10058;
+                    System.out.println("O valor da sua conta é: R$" + conta);
+                } else if (kwh >= 31 && kwh <= 100) {
+                    conta = kwh * 0.17243;
+                    System.out.println("O valor da sua conta é: R$" + conta);
+                } else if (kwh >= 101 && kwh <= 220) {
+                    conta = kwh * 0.25864;
+                    System.out.println("O valor da sua conta é: R$" + conta);
+                } else if (kwh > 220) {
+                    conta = kwh * 0.28738;
+                    System.out.println("O valor da sua conta é: R$" + conta);
+                }
         }
     }
 }
